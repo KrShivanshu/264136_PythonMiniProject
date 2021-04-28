584f6d819d836a47e8024470ac2b1e961bf60fe7
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-29 02:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import read as r
 import write as w
 import purchase as p
+import addProduct as ap
 
-NewCustomerWaiting = "y"
-while NewCustomerWaiting.lower()=="y":
-    a = r.readList()
-    b = p.product(a)
-    w.updateList(a,b)
-    NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
-print("\nThankyou for shopping, Please visit again!")
-print("Please check the invoice created")
+op = int(input("1: Add product\n2: Make bill\nEnter yopur choice: "))
+if 1==op:
+    print("Add product")
+    AddNewProduct = "y"
+    while AddNewProduct.lower()=="y":
+        ap.addproduct()
+        AddNewProduct = input("Do you want to add more products?(y/n)")
+    print("Products added :)")
+        
+elif 2==op:
+    NewCustomerWaiting = "y"
+    while NewCustomerWaiting.lower()=="y":
+        a = r.readList()
+        b = p.product(a)
+        w.updateList(a,b)
+        NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
+    print("\nThankyou for shopping, Please visit again!")
+    print("Please check the invoice created")
