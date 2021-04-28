44cda557685a533cc69d4be01376c53c3d4c8638
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-29 03:08:58 +0530

    Update addProduct.py

--- addProduct.py
+++ addProduct.py
@@ -6,7 +6,17 @@ def addproduct():
         L.append(line.replace("\n", "").split(","))
     file.close()
 
-    L.append([input("Enter the product name: "),(input("Enter the price: ")),(input("Enter the quantity: "))])
+    p_name = input("Enter the product name: ")
+    p_price = int(input("Enter the price: "))
+    p_quantity = int(input("Enter the quantity: "))
+
+    for l in L:
+        if str(l[0]).upper()==p_name.upper():
+            l[1]=str(p_price)
+            l[2]=str(int(l[2])+int(p_quantity))
+            break
+        elif str(L[-1][0]).upper()==str(l[0]).upper():
+            L.append([p_name,str(p_price),str(p_quantity)])
     
     files = open("ProductList.txt", "w") 
     for each in L:
