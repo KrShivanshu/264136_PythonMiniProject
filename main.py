d02bbc9713b39aa600e6c8f30f86facd6ce9bbc7
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-28 23:51:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import read as r
 NewCustomerWaiting = "y"
 while NewCustomerWaiting.lower()=="y":
     a = r.readList()
-    b = purchase.product()
+    b = purchase.product(a)
     write.updateList(a,b)
     NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
 rint("\nThankyou for shopping, Please visit again!")
