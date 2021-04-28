ec01f9af4fea7753cf036e24d283a4e1a709884e
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-28 23:40:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import read as r
+
+NewCustomerWaiting = "y"
+while NewCustomerWaiting.lower()=="y":
+    a = r.readList()
+    b = purchase.product()
+    write.updateList(a,b)
+    NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
+rint("\nThankyou for shopping, Please visit again!")
+print("Please check the invoice created")
