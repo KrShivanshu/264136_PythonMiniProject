ce9a3843c868a43a714a21b916e283d89e1e7f04
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-05-01 03:15:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ import write as w
 import purchase as p
 import addProduct as ap
 
-op = int(input("1: Add product\n2: Make bill\nEnter your choice: "))
-if 1==op:
-    print("\nAdd product\n")
-    AddNewProduct = "y"
-    while AddNewProduct.lower()=="y":
-        ap.addproduct()
-        AddNewProduct = input("\nDo you want to add more products?(y/n)\n")
-    print("Products added :)")
-        
-elif 2==op:
-    NewCustomerWaiting = "y"
-    while NewCustomerWaiting.lower()=="y":
-        a = r.readList()
-        b = p.product(a)
-        w.updateList(a,b)
-        print("Invoice created\n")
-        NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
-    print("\nThankyou for shopping, Please visit again!")
+MakeChoice = "y"
+while MakeChoice.lower()=="y":
+    MakeChoice = "n"
+    op = input("1: Add product\n2: Make bill\nEnter your choice: ")
+    if '1'==str(op):
+        print("\nAdd product\n")
+        AddNewProduct = "y"
+        while AddNewProduct.lower()=="y":
+            ap.addproduct()
+            AddNewProduct = input("\nDo you want to add more products?(y/n)\n")
+        print("Products added :)")
+            
+    elif '2'==str(op):
+        NewCustomerWaiting = "y"
+        while NewCustomerWaiting.lower()=="y":
+            a = r.readList()
+            b = p.product(a)
+            w.updateList(a,b)
+            print("Invoice created\n")
+            NewCustomerWaiting=input("Is any new customer waiting in the queue?(y/n): ")
+        print("\nThankyou for shopping, Please visit again!")
+
+    else:
+        print("Invalid input! Please enter a valid input.")
+        MakeChoice = "y"
